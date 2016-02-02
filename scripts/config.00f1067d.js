1373b2f5d33b68391e0aa8423e5d51ce68815ee2
Jakub Nabrdalik <jakubn@gmail.com>
2016-02-02 18:28:08 +0100

    Updates

--- scripts/config.00f1067d.js
+++ scripts/config.00f1067d.js
@@ -4,7 +4,7 @@
 
 .constant('ENV', 'production')
 
-.constant('CONFERENCE', {id:8,base:'pl',name:'GR8Day Warsaw 2016',contact:'warsaw@gr8day.pl',blogUrl:'softwaremill.com/blog',blogApiKey:'7rXNPh2ZCj2xa12qknvyQyBl1foeXM9q9CThjcQyf3JsTqEZ1I',registrationUrl:'http://flexbillet.dk/gr8conf/event/43814266743758783807ecda53d26b40c417f64d07667f91',analyticsId:'UA-72648941-1',login:true,favorites:true,ratings:true})
+.constant('CONFERENCE', {id:8,base:'pl',name:'GR8Day Warsaw 2016',contact:'gr8day-warsaw@googlegroups.com',blogUrl:'softwaremill.com/blog',blogApiKey:'7rXNPh2ZCj2xa12qknvyQyBl1foeXM9q9CThjcQyf3JsTqEZ1I',registrationUrl:'http://flexbillet.dk/gr8conf/event/43814266743758783807ecda53d26b40c417f64d07667f91',analyticsId:'UA-72648941-1',login:true,favorites:true,ratings:true})
 
 .constant('API', {reload:true,status:'http://cfp.gr8conf.org/api2/status/',speakers:'http://cfp.gr8conf.org/api2/speakers/',talks:'http://cfp.gr8conf.org/api2/talks/',tags:'http://cfp.gr8conf.org/api2/tags/',agenda:'http://cfp.gr8conf.org/api2/agenda/',getCode:'http://cfp.gr8conf.org/api/user/code',getUser:'http://cfp.gr8conf.org/api/user/get',loadFavorites:'http://cfp.gr8conf.org/api/favorites/get',saveFavorite:'http://cfp.gr8conf.org/api/favorites/put',loadRatings:'http://cfp.gr8conf.org/api/ratings/get',saveRating:'http://cfp.gr8conf.org/api/ratings/put',loadConferenceRating:'http://cfp.gr8conf.org/api/confrating/get',saveConferenceRating:'http://cfp.gr8conf.org/api/confrating/put'})
 
